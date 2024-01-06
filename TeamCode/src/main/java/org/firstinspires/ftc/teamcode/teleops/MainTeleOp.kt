f5584c7aba2a0c4af122df7f76069efec9b77df3
PMMEnergy <jwm@sonic.net>
2024-01-05 18:47:47 -0800

    Reorganize more teleops

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/MainTeleOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/MainTeleOp.kt
@@ -1,4 +1,56 @@
 package org.firstinspires.ftc.teamcode.teleops
 
-class MainTeleOp {
+import com.acmerobotics.roadrunner.Pose2d
+import com.acmerobotics.roadrunner.PoseVelocity2d
+import com.acmerobotics.roadrunner.Twist2d
+import com.acmerobotics.roadrunner.Vector2d
+import com.arcrobotics.ftclib.command.CommandOpMode
+import com.arcrobotics.ftclib.gamepad.GamepadEx
+import org.firstinspires.ftc.teamcode.commands.drive.MotionProfiledJoystickDrive
+import org.firstinspires.ftc.teamcode.commands.drive.SimpleJoystickDrive
+import org.firstinspires.ftc.teamcode.subsystems.Claw
+import org.firstinspires.ftc.teamcode.subsystems.Lift
+import org.firstinspires.ftc.teamcode.subsystems.MecanumDrive
+import org.firstinspires.ftc.teamcode.subsystems.Passthrough
+import org.firstinspires.ftc.teamcode.subsystems.Vision
+import org.firstinspires.ftc.teamcode.subsystems.VoltageSensor
+import org.firstinspires.ftc.teamcode.subsystems.localization.OdometryLocalizer
+
+class MainTeleOp  : CommandOpMode() {
+    override fun initialize() {
+        /****************************************************
+         * Initialize hardware                              *
+         ****************************************************/
+
+        val voltageSensor = VoltageSensor(hardwareMap)
+        val lift = Lift(hardwareMap, voltageSensor)
+        val claw = Claw(hardwareMap)
+        val passthrough = Passthrough(hardwareMap)
+        val vision = Vision(hardwareMap)
+        //val localizer = MecanumMonteCarloLocalizer(hardwareMap, vision, Pose2d(), arrayToRowMatrix(doubleArrayOf()))
+        val localizer = OdometryLocalizer(hardwareMap)
+        val mecanum = MecanumDrive(hardwareMap, Pose2d(0.0, 0.0, Math.toRadians(90.0)), localizer, voltageSensor)
+
+        //register(lift, claw, passthrough, mecanum, vision)
+        register(mecanum, claw)
+
+        /****************************************************
+         * Driver 1 Controls                                *
+         * Driving and semi autonomous control              *
+         ****************************************************/
+
+        val driver1 = GamepadEx(gamepad1)
+
+        /**
+         * Drive
+         */
+        val input = { PoseVelocity2d(Vector2d(driver1.leftY, -driver1.leftX), -driver1.rightX) }
+
+        var fieldCentric = true
+        val fieldCentricProvider = { fieldCentric }
+
+        mecanum.defaultCommand = SimpleJoystickDrive(mecanum, input, fieldCentricProvider)
+
+    }
+
 }
