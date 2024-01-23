546435dc174ae9450fe8b68594c5c1a2699534e6
PMMEnergy <jwm@sonic.net>
2024-01-22 17:46:35 -0800

    Auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/auto/AutoTrajectory.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.teleops.auto;
+
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.subsystems.MecanumDrive;
+import org.firstinspires.ftc.teamcode.subsystems.VoltageSensor;
+import org.firstinspires.ftc.teamcode.subsystems.localization.OdometryLocalizer;
+
+@Autonomous
+public class AutoTrajectory extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        OdometryLocalizer localizer = new OdometryLocalizer(hardwareMap);
+        VoltageSensor voltageSensor = new VoltageSensor(hardwareMap);
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(-45,-36,0), localizer, voltageSensor, telemetry);
+
+        Action blueRight = drive.actionBuilder(new Pose2d(-45, -36, 0))
+                .setTangent(180)
+                .splineTo(new Vector2d(-60, 12), 90)
+                .splineTo(new Vector2d(-36, 36), 90)
+                .splineToConstantHeading(new Vector2d(-60, 48), 90)
+                .splineToConstantHeading(new Vector2d(-60, 60), 90)
+                .build();
+
+        waitForStart();
+
+        if(isStopRequested()) return;
+
+        Actions.runBlocking(blueRight);
+    }
+}
