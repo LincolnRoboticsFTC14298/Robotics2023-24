ff842f86ec0988cdc5a8c4514af9e3811ecc7258
PMMEnergy <jwm@sonic.net>
2024-01-24 11:26:15 -0800

    Fixed degrees into radians

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/auto/AutoTrajectory.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/auto/AutoTrajectory.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.teleops.auto;
 
+import static java.lang.Math.toRadians;
+
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.acmerobotics.roadrunner.Pose2d;
@@ -12,53 +14,53 @@ public class AutoTrajectory extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
-        //Pose2d blueRightStartingPose = new Pose2d(-45, -36, 0);    //old version (no 1st part)
+        //Pose2d blueRightStartingPose = new Pose2d(-45, -36, toRadians(0));    //old version (no 1st part)
 
-        Pose2d blueRightStartingPose = new Pose2d(-63, -36, -180);
-        Pose2d blueLeftStartingPose = new Pose2d(-45, 12, 0);
-        Pose2d redLeftStartingPose = new Pose2d(45, -36, 180);
-        Pose2d redRightStartingPose = new Pose2d(45, 12, 180);
+        Pose2d blueRightStartingPose = new Pose2d(-63, -36, toRadians(-180));
+        Pose2d blueLeftStartingPose = new Pose2d(-45, 12, toRadians(0));
+        Pose2d redLeftStartingPose = new Pose2d(45, -36, toRadians(180));
+        Pose2d redRightStartingPose = new Pose2d(45, 12, toRadians(180));
         MecanumDrive drive = new MecanumDrive(hardwareMap, blueRightStartingPose);
 
-//        Action blueRight = drive.actionBuilder(blueRightStartingPose)
-//                .setTangent(180)
-//                .splineToLinearHeading(new Pose2d(-60, 12, -90), 90)
-//                .splineToLinearHeading(new Pose2d(-36, 36, -90), 90)
-//                .splineToConstantHeading(new Vector2d(-60, 48), 90)
-//                .splineToConstantHeading(new Vector2d(-60, 60), 90)
+//        Action blueRight = drive.actionBuilder(blueRightStartingPose) //old version (no 1st part)
+//                .setTangent(toRadians(180))
+//                .splineToLinearHeading(new Pose2d(-60, 12, toRadians(-90)), toRadians(90))
+//                .splineToLinearHeading(new Pose2d(-36, 36, toRadians(-90)), toRadians(90))
+//                .splineToConstantHeading(new Vector2d(-60, 48), toRadians(90))
+//                .splineToConstantHeading(new Vector2d(-60, 60), toRadians(90))
 //                .build();
-//        old version (no 1st part)
+
 
         Action blueRight = drive.actionBuilder(blueRightStartingPose)
-                .setTangent(-90)
-                .splineToConstantHeading(new Vector2d(-57, -42), 0)
-                .splineToLinearHeading(new Pose2d(-45, -36, 0), 180)
-                .splineToLinearHeading(new Pose2d(-60, 12, -90), 90)
-                .splineToLinearHeading(new Pose2d(-36, 36, -90), 90)
-                .splineToConstantHeading(new Vector2d(-60, 48), 90)
-                .splineToConstantHeading(new Vector2d(-60, 60), 90)
+                .setTangent(toRadians(-90))
+                .splineToConstantHeading(new Vector2d(-57, -42), toRadians(0))
+                .splineToLinearHeading(new Pose2d(-45, -36, toRadians(0)), toRadians(180))
+                .splineToLinearHeading(new Pose2d(-60, 12, toRadians(-90)), toRadians(90))
+                .splineToLinearHeading(new Pose2d(-36, 36, toRadians(-90)), toRadians(90))
+                .splineToConstantHeading(new Vector2d(-60, 48), toRadians(90))
+                .splineToConstantHeading(new Vector2d(-60, 60), toRadians(90))
                 .build();
 
         Action blueLeft = drive.actionBuilder(blueLeftStartingPose)
-                .setTangent(90)
-                .splineToLinearHeading(new Pose2d(-36, 36, -90), 90)
-                .splineToConstantHeading(new Vector2d(-60, 48), 90)
-                .splineToConstantHeading(new Vector2d(-60, 60), 90)
+                .setTangent(toRadians(90))
+                .splineToLinearHeading(new Pose2d(-36, 36, toRadians(-90)), toRadians(90))
+                .splineToConstantHeading(new Vector2d(-60, 48), toRadians(90))
+                .splineToConstantHeading(new Vector2d(-60, 60), toRadians(90))
                 .build();
 
         Action redLeft = drive.actionBuilder(redLeftStartingPose)
-                .setTangent(0)
-                .splineToLinearHeading(new Pose2d(60, 12, -90), 90)
-                .splineToLinearHeading(new Pose2d(36, 36, -90), 90)
-                .splineToConstantHeading(new Vector2d(60, 48), 90)
-                .splineToConstantHeading(new Vector2d(60, 60), 90)
+                .setTangent(toRadians(0))
+                .splineToLinearHeading(new Pose2d(60, 12, toRadians(-90)), toRadians(90))
+                .splineToLinearHeading(new Pose2d(36, 36, toRadians(-90)), toRadians(90))
+                .splineToConstantHeading(new Vector2d(60, 48), toRadians(90))
+                .splineToConstantHeading(new Vector2d(60, 60), toRadians(90))
                 .build();
 
         Action redRight = drive.actionBuilder(redRightStartingPose)
-                .setTangent(90)
-                .splineToLinearHeading(new Pose2d(36, 36, -90), 90)
-                .splineToConstantHeading(new Vector2d(60, 48), 90)
-                .splineToConstantHeading(new Vector2d(60, 60), 90)
+                .setTangent(toRadians(90))
+                .splineToLinearHeading(new Pose2d(36, 36, toRadians(-90)), toRadians(90))
+                .splineToConstantHeading(new Vector2d(60, 48), toRadians(90))
+                .splineToConstantHeading(new Vector2d(60, 60), toRadians(90))
                 .build();
 
         waitForStart();
