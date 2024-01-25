b6e0a22caa823d1d8c29e371778e6b47bdb1e7d0
PMMEnergy <jwm@sonic.net>
2024-01-25 15:56:07 -0800

    Made switch case for trajectory selection :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/auto/AutoTrajectoryKotlin.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/auto/AutoTrajectoryKotlin.kt
@@ -5,6 +5,7 @@ import com.acmerobotics.roadrunner.Vector2d
 import com.acmerobotics.roadrunner.ftc.runBlocking
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import org.firstinspires.ftc.teamcode.subsystems.localization.StartingPoseStorage
 
 @Autonomous
 class AutoTrajectoryKotlin : LinearOpMode() {
@@ -15,54 +16,57 @@ class AutoTrajectoryKotlin : LinearOpMode() {
         RED_RIGHT(Pose2d(63.0, 12.0, Math.toRadians(0.0)))
     }
 
+
+    val drive = MecanumDrive(hardwareMap, StartingPoseStorage.startingPose.pose)
+
+    val blueRight = drive.actionBuilder(drive.pose)
+        .setTangent(Math.toRadians(-90.0))
+        .splineToConstantHeading(Vector2d(-57.0, -42.0), Math.toRadians(0.0))
+        .splineToLinearHeading(Pose2d(-45.0, -36.0, Math.toRadians(0.0)), Math.toRadians(180.0))
+        .splineToLinearHeading(Pose2d(-60.0, 12.0, Math.toRadians(-90.0)), Math.toRadians(90.0))
+        .splineToLinearHeading(Pose2d(-36.0, 36.0, Math.toRadians(-90.0)), Math.toRadians(90.0))
+        .splineToConstantHeading(Vector2d(-63.0, 48.0), Math.toRadians(90.0))
+        .splineToConstantHeading(Vector2d(-63.0, 60.0), Math.toRadians(90.0))
+        .build()
+
+    val blueLeft = drive.actionBuilder(drive.pose)
+        .setTangent(Math.toRadians(90.0))
+        .splineToConstantHeading(Vector2d(-57.0, 18.0), Math.toRadians(0.0))
+        .splineToLinearHeading(Pose2d(-45.0, 12.0, 0.0), Math.toRadians(-15.0))
+        .splineToLinearHeading(Pose2d(-36.0, 36.0, Math.toRadians(-90.0)), Math.toRadians(90.0))
+        .splineToConstantHeading(Vector2d(-63.0, 48.0), Math.toRadians(90.0))
+        .splineToConstantHeading(Vector2d(-63.0, 60.0), Math.toRadians(90.0))
+        .build()
+
+    val redLeft = drive.actionBuilder(drive.pose)
+        .setTangent(Math.toRadians(-90.0))
+        .splineToConstantHeading(Vector2d(57.0, -42.0), Math.toRadians(180.0))
+        .splineToLinearHeading(Pose2d(45.0, -36.0, Math.toRadians(180.0)), Math.toRadians(0.0))
+        .splineToLinearHeading(Pose2d(60.0, 12.0, Math.toRadians(-90.0)), Math.toRadians(90.0))
+        .splineToLinearHeading(Pose2d(36.0, 36.0, Math.toRadians(-90.0)), Math.toRadians(90.0))
+        .splineToConstantHeading(Vector2d(63.0, 48.0), Math.toRadians(90.0))
+        .splineToConstantHeading(Vector2d(63.0, 60.0), Math.toRadians(90.0))
+        .build()
+
+    val redRight = drive.actionBuilder(drive.pose)
+        .setTangent(Math.toRadians(90.0))
+        .splineToConstantHeading(Vector2d(57.0, 18.0), Math.toRadians(180.0))
+        .splineToLinearHeading(Pose2d(45.0, 12.0, 180.0), Math.toRadians(165.0))
+        .splineToLinearHeading(Pose2d(36.0, 36.0, Math.toRadians(90.0)), Math.toRadians(90.0))
+        .splineToConstantHeading(Vector2d(63.0, 48.0), Math.toRadians(90.0))
+        .splineToConstantHeading(Vector2d(63.0, 60.0), Math.toRadians(90.0))
+        .build()
+
     override fun runOpMode() {
-        val drive = MecanumDrive(hardwareMap, StartingPose.BLUE_LEFT.pose)
-
-        val blueRight = drive.actionBuilder(drive.pose)
-                .setTangent(Math.toRadians(-90.0))
-                .splineToConstantHeading(Vector2d(-57.0, -42.0), Math.toRadians(0.0))
-                .splineToLinearHeading(Pose2d(-45.0, -36.0, Math.toRadians(0.0)), Math.toRadians(180.0))
-                .splineToLinearHeading(Pose2d(-60.0, 12.0, Math.toRadians(-90.0)), Math.toRadians(90.0))
-                .splineToLinearHeading(Pose2d(-36.0, 36.0, Math.toRadians(-90.0)), Math.toRadians(90.0))
-                .splineToConstantHeading(Vector2d(-63.0, 48.0), Math.toRadians(90.0))
-                .splineToConstantHeading(Vector2d(-63.0, 60.0), Math.toRadians(90.0))
-                .build()
-
-        val blueLeft = drive.actionBuilder(drive.pose)
-                .setTangent(Math.toRadians(90.0))
-                .splineToConstantHeading(Vector2d(-57.0, 18.0), Math.toRadians(0.0))
-                .splineToLinearHeading(Pose2d(-45.0, 12.0, 0.0), Math.toRadians(-15.0))
-                .splineToLinearHeading(Pose2d(-36.0, 36.0, Math.toRadians(-90.0)), Math.toRadians(90.0))
-                .splineToConstantHeading(Vector2d(-63.0, 48.0), Math.toRadians(90.0))
-                .splineToConstantHeading(Vector2d(-63.0, 60.0), Math.toRadians(90.0))
-                .build()
-
-        val redLeft = drive.actionBuilder(drive.pose)
-                .setTangent(Math.toRadians(-90.0))
-                .splineToConstantHeading(Vector2d(57.0, -42.0), Math.toRadians(180.0))
-                .splineToLinearHeading(Pose2d(45.0, -36.0, Math.toRadians(180.0)), Math.toRadians(0.0))
-                .splineToLinearHeading(Pose2d(60.0, 12.0, Math.toRadians(-90.0)), Math.toRadians(90.0))
-                .splineToLinearHeading(Pose2d(36.0, 36.0, Math.toRadians(-90.0)), Math.toRadians(90.0))
-                .splineToConstantHeading(Vector2d(63.0, 48.0), Math.toRadians(90.0))
-                .splineToConstantHeading(Vector2d(63.0, 60.0), Math.toRadians(90.0))
-                .build()
-
-        val redRight = drive.actionBuilder(drive.pose)
-                .setTangent(Math.toRadians(90.0))
-                .splineToConstantHeading(Vector2d(57.0, 18.0), Math.toRadians(180.0))
-                .splineToLinearHeading(Pose2d(45.0, 12.0, 180.0), Math.toRadians(165.0))
-                .splineToLinearHeading(Pose2d(36.0, 36.0, Math.toRadians(90.0)), Math.toRadians(90.0))
-                .splineToConstantHeading(Vector2d(63.0, 48.0), Math.toRadians(90.0))
-                .splineToConstantHeading(Vector2d(63.0, 60.0), Math.toRadians(90.0))
-                .build()
-
         waitForStart()
 
         if (isStopRequested) return
 
-        runBlocking(blueRight)
-//        runBlocking(blueLeft)
-//        runBlocking(redLeft)
-//        runBlocking(redRight)
+        when (StartingPoseStorage.startingPose) {
+            StartingPose.BLUE_RIGHT -> runBlocking(blueRight)
+            StartingPose.BLUE_LEFT -> runBlocking(blueLeft)
+            StartingPose.RED_RIGHT -> runBlocking(redRight)
+            StartingPose.RED_LEFT -> runBlocking(redLeft)
+        }
     }
 }
