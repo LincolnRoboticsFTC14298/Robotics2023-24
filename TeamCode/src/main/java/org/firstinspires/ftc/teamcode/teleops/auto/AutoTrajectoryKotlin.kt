b59cdfac4d97fa9dab5030565fb29615fd6cb386
Lions123 <pee@poo.cum>
2024-01-26 21:47:33 -0800

    Interrogating Trajectories

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/auto/AutoTrajectoryKotlin.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/auto/AutoTrajectoryKotlin.kt
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.teleops.auto
 
+import java.lang.Math.toRadians
+
 import com.acmerobotics.roadrunner.Pose2d
 import com.acmerobotics.roadrunner.Vector2d
 import com.acmerobotics.roadrunner.ftc.runBlocking
@@ -8,53 +10,54 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import org.firstinspires.ftc.teamcode.subsystems.localization.StartingPose
 import org.firstinspires.ftc.teamcode.subsystems.localization.StartingPoseStorage
 
+
 @Autonomous
 class AutoTrajectoryKotlin : LinearOpMode() {
     override fun runOpMode() {
         val drive = MecanumDrive(hardwareMap, StartingPoseStorage.startingPose.pose)
 
         val blueRight = drive.actionBuilder(drive.pose)
-            .setTangent(Math.toRadians(-90.0))
-            .splineToConstantHeading(Vector2d(-57.0, -42.0), Math.toRadians(0.0))
-            .splineToLinearHeading(Pose2d(-45.0, -36.0, Math.toRadians(0.0)), Math.toRadians(180.0))
-            .splineToLinearHeading(Pose2d(-60.0, -48.0, Math.toRadians(-90.0)), Math.toRadians(-90.0))
-            .splineToLinearHeading(Pose2d(-60.0, 12.0, Math.toRadians(-90.0)), Math.toRadians(90.0))
-            .splineToLinearHeading(Pose2d(-36.0, 36.0, Math.toRadians(-90.0)), Math.toRadians(90.0))
-            .splineToConstantHeading(Vector2d(-60.0, 48.0), Math.toRadians(90.0))
-            .splineToConstantHeading(Vector2d(-60.0, 60.0), Math.toRadians(90.0))
+            .setTangent(toRadians(-90.0))
+            .splineToConstantHeading(Vector2d(-57.0, -42.0), toRadians(0.0))
+            .splineToLinearHeading(Pose2d(-45.0, -36.0, toRadians(0.0)), toRadians(180.0))
+            .splineToLinearHeading(Pose2d(-60.0, -48.0, toRadians(-90.0)), toRadians(-90.0))
+            .splineToLinearHeading(Pose2d(-60.0, 12.0, toRadians(-90.0)), toRadians(90.0))
+            .splineToLinearHeading(Pose2d(-36.0, 36.0, toRadians(-90.0)), toRadians(90.0))
+            .splineToConstantHeading(Vector2d(-60.0, 48.0), toRadians(90.0))
+            .splineToConstantHeading(Vector2d(-60.0, 60.0), toRadians(90.0))
             .build()
 
         val blueLeft = drive.actionBuilder(drive.pose)
-            .setTangent(Math.toRadians(90.0))
-            .splineToConstantHeading(Vector2d(-54.0, 24.0), Math.toRadians(45.0))
-            .splineToLinearHeading(Pose2d(-54.0, 18.0, Math.toRadians(0.0)), Math.toRadians(-90.0))
-            .splineToConstantHeading(Vector2d(-45.0, 12.0,), Math.toRadians(90.0))
-            .splineToLinearHeading(Pose2d(-54.0, 36.0, Math.toRadians(-90.0)), Math.toRadians(90.0))
-            .splineToConstantHeading(Vector2d(-36.0, 36.0), Math.toRadians(90.0))
-            .splineToConstantHeading(Vector2d(-60.0, 48.0), Math.toRadians(90.0))
-            .splineToConstantHeading(Vector2d(-60.0, 60.0), Math.toRadians(90.0))
+            .setTangent(toRadians(90.0))
+            .splineToConstantHeading(Vector2d(-54.0, 24.0), toRadians(45.0))
+            .splineToLinearHeading(Pose2d(-54.0, 18.0, toRadians(0.0)), toRadians(-90.0))
+            .splineToConstantHeading(Vector2d(-45.0, 12.0), toRadians(90.0))
+            .splineToLinearHeading(Pose2d(-54.0, 36.0, toRadians(-90.0)), toRadians(90.0))
+            .splineToConstantHeading(Vector2d(-36.0, 36.0), toRadians(90.0))
+            .splineToConstantHeading(Vector2d(-60.0, 48.0), toRadians(90.0))
+            .splineToConstantHeading(Vector2d(-60.0, 60.0), toRadians(90.0))
             .build()
 
         val redLeft = drive.actionBuilder(drive.pose)
-            .setTangent(Math.toRadians(-90.0))
-            .splineToConstantHeading(Vector2d(57.0, -42.0), Math.toRadians(180.0))
-            .splineToLinearHeading(Pose2d(45.0, -36.0, Math.toRadians(180.0)), Math.toRadians(0.0))
-            .splineToLinearHeading(Pose2d(60.0, -48.0, Math.toRadians(-90.0)), Math.toRadians(-90.0))
-            .splineToLinearHeading(Pose2d(60.0, 12.0, Math.toRadians(-90.0)), Math.toRadians(90.0))
-            .splineToLinearHeading(Pose2d(36.0, 36.0, Math.toRadians(-90.0)), Math.toRadians(90.0))
-            .splineToConstantHeading(Vector2d(60.0, 48.0), Math.toRadians(90.0))
-            .splineToConstantHeading(Vector2d(60.0, 60.0), Math.toRadians(90.0))
+            .setTangent(toRadians(-90.0))
+            .splineToConstantHeading(Vector2d(57.0, -42.0), toRadians(180.0))
+            .splineToLinearHeading(Pose2d(45.0, -36.0, toRadians(180.0)), toRadians(0.0))
+            .splineToLinearHeading(Pose2d(60.0, -48.0, toRadians(-90.0)), toRadians(-90.0))
+            .splineToLinearHeading(Pose2d(60.0, 12.0, toRadians(-90.0)), toRadians(90.0))
+            .splineToLinearHeading(Pose2d(36.0, 36.0, toRadians(-90.0)), toRadians(90.0))
+            .splineToConstantHeading(Vector2d(60.0, 48.0), toRadians(90.0))
+            .splineToConstantHeading(Vector2d(60.0, 60.0), toRadians(90.0))
             .build()
 
         val redRight = drive.actionBuilder(drive.pose)
-            .setTangent(Math.toRadians(90.0))
-            .splineToConstantHeading(Vector2d(54.0, 24.0), Math.toRadians(135.0))
-            .splineToLinearHeading(Pose2d(54.0, 18.0, Math.toRadians(180.0)), Math.toRadians(-90.0))
-            .splineToConstantHeading(Vector2d(45.0, 12.0,), Math.toRadians(90.0))
-            .splineToLinearHeading(Pose2d(54.0, 36.0, Math.toRadians(-90.0)), Math.toRadians(90.0))
-            .splineToConstantHeading(Vector2d(36.0, 36.0), Math.toRadians(90.0))
-            .splineToConstantHeading(Vector2d(60.0, 48.0), Math.toRadians(90.0))
-            .splineToConstantHeading(Vector2d(60.0, 60.0), Math.toRadians(90.0))
+            .setTangent(toRadians(90.0))
+            .splineToConstantHeading(Vector2d(54.0, 24.0), toRadians(135.0))
+            .splineToLinearHeading(Pose2d(54.0, 18.0, toRadians(180.0)), toRadians(-90.0))
+            .splineToConstantHeading(Vector2d(45.0, 12.0), toRadians(90.0))
+            .splineToLinearHeading(Pose2d(54.0, 36.0, toRadians(-90.0)), toRadians(90.0))
+            .splineToConstantHeading(Vector2d(36.0, 36.0), toRadians(90.0))
+            .splineToConstantHeading(Vector2d(60.0, 48.0), toRadians(90.0))
+            .splineToConstantHeading(Vector2d(60.0, 60.0), toRadians(90.0))
             .build()
 
         waitForStart()
